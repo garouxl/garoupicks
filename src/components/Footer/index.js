b488bb0465ea3843efb9c4948dc86eefd4e56f33
Leandro Almeida <vempari@gmail.com>
2020-07-31 19:31:25 -0300

    alterado logo inferior

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { FooterBase } from './styles';
-import Logo from '../Logo'
+// import Logo from '../Logo'
+import Logo from '../../assets/img/alura-logo.svg';
 
 function Footer() {
   return (
     <FooterBase>
       <FooterBase.wrapper>
-        <Logo title="Garoupicks lower logo"/>
+      <a href="https://www.alura.com.br/">
+        <img src={Logo} alt="Logo Alura" />
+      </a>
         <p>
           Criado durante a
           {' '}
